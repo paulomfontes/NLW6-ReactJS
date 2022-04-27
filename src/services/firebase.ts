4f9de1ca370235fdbeb870dd63be321d6849452d
Paulo Fontes <paulomfontes@gmail.com>
2022-04-26 21:55:46 -0300

    Update Firebase and Depencies

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import firebase from 'firebase/app';
 
+import 'firebase/auth';
+import 'firebase/database';
 
 
 const firebaseConfig = {
